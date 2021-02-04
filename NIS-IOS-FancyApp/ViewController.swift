580d16d9b7002d4d9e2fdd6d94779a9b71556973
Bsw20 <yaakarpunkin@edu.hse.ru>
2021-02-04 22:07:10 +0300

    Initial Commit

---
+++ NIS-IOS-FancyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NIS-IOS-FancyApp
+//
+//  Created by Ярослав Карпунькин on 04.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
